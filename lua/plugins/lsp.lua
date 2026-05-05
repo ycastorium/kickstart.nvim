896ca34b796a5c1fdb26cdec56d94519773032e6
Cλstor <ygorcastor@gmail.com>
2026-05-05 21:03:23 +0200

    Fixed deprecated function on LSP

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -288,7 +288,7 @@ return {
         callback = function(event)
           local client = vim.lsp.get_client_by_id(event.data.client_id)
 
-          if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+          if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
             local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
             vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
               buffer = event.buf,
